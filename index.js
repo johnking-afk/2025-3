8948fbf9bb2b270f3e87b515f98a12c2551bde5d
Your Name <yamacarkan@seattleacademy.org>
2020-03-03 11:01:13 -0800

    Install express.js and facecam api

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 1527
+const attendance = [];
+
+
+app.get('/', (req, res) => {
+	let person = req.query.name;
+	attendance.push(person);
+	console.log(attendance)
+	res.send(attendance)
+})
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
